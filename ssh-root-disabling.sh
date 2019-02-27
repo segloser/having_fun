aa38a9feeec150f25e92ba88c152554c199fef49
Segloser <12746414+segloser@users.noreply.github.com>
2019-02-27 20:32:48 +0100

    Create ssh-root-disabling.sh

---
+++ ssh-root-disabling.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Disabling PermitRootLogin yes and PasswordAuthentication yes"
+
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+
+echo "Restarting ssh service"
+service ssh restart
