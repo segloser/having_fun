0e980889aacfe3c19191c52aba71f64e89be0724
Segloser <12746414+segloser@users.noreply.github.com>
2019-12-19 16:56:27 +0100

    Add files via upload

---
+++ PowerShell/progressive-assimilation/4.ps1
@@ -0,0 +1,20 @@
+﻿# This is a comment in PowerShell
+Write-Host "Welcome to my first simple PowerShell script"
+
+# Cleaning the screen now
+cls
+
+# Setting your first variable in PowerShell
+$just_var = "I am a just a test variable"
+
+# Writing the variable on the screen
+$just_var
+
+# Now I will require information from the user asking a question
+# and that information will be stored in the variable <username>
+$username = Read-Host "`nPlease, type your name"
+
+# Introducing a temporary interruption or pause until pressing ENTER
+pause
+
+Write-Host "Hello $username, now we will need your password"
